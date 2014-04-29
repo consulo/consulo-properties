0e6febff5f62c3cb98cd2ac5fdd1b7e9cd4795f0
VISTALL <vistall.valeriy@gmail.com>
2014-04-29 20:15:11 +0300

    FileTypeWithPredefinedCharset

--- src/com/intellij/lang/properties/PropertiesFileType.java
+++ src/com/intellij/lang/properties/PropertiesFileType.java
@@ -15,55 +15,74 @@
  */
 package com.intellij.lang.properties;
 
+import java.nio.charset.Charset;
+
+import javax.swing.Icon;
+
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.fileTypes.FileTypeWithPredefinedCharset;
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.CharsetToolkit;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.encoding.EncodingManager;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-import java.nio.charset.Charset;
 
 /**
  * @author max
  */
-public class PropertiesFileType extends LanguageFileType {
-  public static final LanguageFileType INSTANCE = new PropertiesFileType();
-  @NonNls public static final String DEFAULT_EXTENSION = "properties";
-  @NonNls public static final String DOT_DEFAULT_EXTENSION = "."+DEFAULT_EXTENSION;
+public class PropertiesFileType extends LanguageFileType implements FileTypeWithPredefinedCharset
+{
+	public static final LanguageFileType INSTANCE = new PropertiesFileType();
+	@NonNls
+	public static final String DEFAULT_EXTENSION = "properties";
+	@NonNls
+	public static final String DOT_DEFAULT_EXTENSION = "." + DEFAULT_EXTENSION;
 
-  private PropertiesFileType() {
-    super(PropertiesLanguage.INSTANCE);
-  }
+	private PropertiesFileType()
+	{
+		super(PropertiesLanguage.INSTANCE);
+	}
 
-  @Override
-  @NotNull
-  public String getName() {
-    return "Properties";
-  }
+	@Override
+	@NotNull
+	public String getName()
+	{
+		return "Properties";
+	}
 
-  @Override
-  @NotNull
-  public String getDescription() {
-    return PropertiesBundle.message("properties.files.file.type.description");
-  }
+	@Override
+	@NotNull
+	public String getDescription()
+	{
+		return PropertiesBundle.message("properties.files.file.type.description");
+	}
 
-  @Override
-  @NotNull
-  public String getDefaultExtension() {
-    return DEFAULT_EXTENSION;
-  }
+	@Override
+	@NotNull
+	public String getDefaultExtension()
+	{
+		return DEFAULT_EXTENSION;
+	}
 
-  @Override
-  public Icon getIcon() {
-    return AllIcons.FileTypes.Properties;
-  }
+	@Override
+	public Icon getIcon()
+	{
+		return AllIcons.FileTypes.Properties;
+	}
 
-  @Override
-  public String getCharset(@NotNull VirtualFile file, final byte[] content) {
-    Charset charset = EncodingManager.getInstance().getDefaultCharsetForPropertiesFiles(file);
-    return charset == null ? CharsetToolkit.getDefaultSystemCharset().name() : charset.name();
-  }
+	@Override
+	public String getCharset(@NotNull VirtualFile file, final byte[] content)
+	{
+		Charset charset = EncodingManager.getInstance().getDefaultCharsetForPropertiesFiles(file);
+		return charset == null ? CharsetToolkit.getDefaultSystemCharset().name() : charset.name();
+	}
+
+	@NotNull
+	@Override
+	public Pair<Charset, String> getPredefinedCharset(@NotNull VirtualFile virtualFile)
+	{
+		return Pair.create(virtualFile.getCharset(), PropertiesBundle.message("properties.files.file.type.description"));
+	}
 }
