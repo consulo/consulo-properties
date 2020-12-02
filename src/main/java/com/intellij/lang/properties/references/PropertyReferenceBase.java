88f5523cb8f8fb169198f287724ae48a6c90dc9b
VISTALL <vistall.valeriy@gmail.com>
2020-12-02 18:47:52 +0300

    compilation

--- src/main/java/com/intellij/lang/properties/references/PropertyReferenceBase.java
+++ src/main/java/com/intellij/lang/properties/references/PropertyReferenceBase.java
@@ -32,7 +32,6 @@ import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.NullableFunction;
 import com.intellij.util.containers.ContainerUtil;
-import consulo.awt.TargetAWT;
 import gnu.trove.THashSet;
 import gnu.trove.TObjectHashingStrategy;
 
@@ -87,7 +86,7 @@ public abstract class PropertyReferenceBase implements PsiPolyVariantReference, 
       }
 
       TextAttributes attrs = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(PropertiesHighlighter.PROPERTY_VALUE);
-      presentation.setTailText(value, TargetAWT.to(attrs.getForegroundColor()));
+      presentation.setTailText(value, attrs.getForegroundColor());
     }
   };
   protected final String myKey;
